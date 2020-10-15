bef3a72f4172c997fbb8329aa0c644690a705f5d
Marcos Henrique Curtale Serafim <marcos.serafim@pampa.compasso>
2020-10-15 10:05:23 -0300

    Numeric methods version 2 - 2020

---
+++ numeric_methods_v2/newton-method/newton_method.py
@@ -0,0 +1,34 @@
+import math
+
+class Newton_method:
+
+    def __init__(self, x):
+        self.x = float(x)
+        self.error = 99
+
+    def function(self):
+        return math.cos(self.x) - math.pow(self.x, 2)
+
+    def derivative(self):
+        return -math.sin(self.x) - 2*self.x
+
+    def interate(self):
+        xn = self.x - (self.function() / self.derivative())
+        self.error = abs(self.x - xn)
+        self.x = xn
+        return xn
+
+if __name__ == '__main__':
+    x0 = 1
+    tol = 1e-5
+    it = 0
+    root_found = None
+   
+    newton = Newton_method(x0)
+    
+    while(newton.error >= tol):
+        print('Iteracao ' + str(it+1))
+        root_found = newton.interate()
+        print(root_found)
+        it += 1
+    
